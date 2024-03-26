ba5f09192018579d7b2cf6b4290dda9530ef8707
Joel McCoy <nbajoel@gmail.com>
2024-03-26 11:01:01 -0500

    added some missing things

--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,6 @@
-from core.views import home
 from django.contrib import admin
 from django.urls import path
+
+from core.views import home
 
 urlpatterns = [path("supersecretadmin/", admin.site.urls), path("", home, name="home")]
