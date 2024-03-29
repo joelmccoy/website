e5639c54cfb001d064b0386df7c5677737dde856
Joel McCoy <nbajoel@gmail.com>
2024-03-29 10:56:08 -0500

    admin template holder pages

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from django.urls import path
 
-from core.views import home
+from core.views import home, about, experience, skills, projects, contact
 import os
 
 admin_path = os.getenv("ADMIN_PATH", "supersecretadmin")
-urlpatterns = [path(admin_path, admin.site.urls), path("", home, name="home")]
+urlpatterns = [
+    path(admin_path, admin.site.urls),
+    path("", home, name="home"),
+    path("home", home, name="home"),
+    path("about", about, name="about"),
+    path("experience", experience, name="experience"),
+    path("skills", skills, name="skills"),
+    path("projects", projects, name="projects"),
+    path("contact", contact, name="contact"),
+]
